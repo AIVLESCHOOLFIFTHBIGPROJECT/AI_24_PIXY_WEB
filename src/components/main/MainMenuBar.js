1ca175c341aec107022dc642d039268bf9e3de64
rojaehoon <wogns5493@gmail.com>
2024-07-15 14:33:27 +0900

    add sales and inquiry

--- src/components/main/MainMenuBar.js
+++ src/components/main/MainMenuBar.js
@@ -7,7 +7,9 @@ const MainMenuBar = () => {
       <ul>
         <li><Link to="/main/dashboard">Dashboard</Link></li>
         <li><Link to="/main/pixycustom">PixyCustom</Link></li>
+        <li><Link to="/main/sales">Sales</Link></li>
         <li><Link to="/main/cctv">CCTV</Link></li>
+        <li><Link to="/main/inquiry">Inquiry</Link></li>
         <li><Link to="/main/notice">Notice</Link></li>
         <li><Link to="/main/settings">Settings</Link></li>
       </ul>
---
+++ src/pages/main/Inquiry.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Inquiry = () => {
+  return <div>Inquiry Page</div>;
+};
+
+export default Inquiry;
