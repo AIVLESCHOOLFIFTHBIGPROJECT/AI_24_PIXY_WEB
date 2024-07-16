91e560e5e1519248a5b7c27087985c07f38b50c2
unduck <csc7545@naver.com>
2024-07-16 16:38:58 +0900

    [UPDATE] 헤더 변경

--- src/components/main/MainHeader.js
+++ src/components/main/MainHeader.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
-import logo from '../../assets/logo.svg';
+import logoBlack from '../../assets/logo-black.svg';
 
 const MainHeader = () => {
   return (
     <AppBar position='fixed' color="default" elevation={0}>
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <img src={logo} alt="Pixy Logo" height="30" />
+          <img src={logoBlack} alt="Pixy Logo" height="30" />
         </Typography>
         <Box>
           <Button color="inherit" component={Link} to="/main/notifications">알림</Button>
