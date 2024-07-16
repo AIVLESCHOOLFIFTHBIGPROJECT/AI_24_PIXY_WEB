2df468de4d30f660cfeb44630e681740900b4a3e
unduck <csc7545@naver.com>
2024-07-16 14:31:09 +0900

    [UPDATE] 홈페이지 타이포 변경

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -11,21 +11,33 @@ const HeroSection = () => {
         backgroundPosition: 'center',
         height: '100vh',
         display: 'flex',
+        justifyContent: 'center',
         alignItems: 'center',
         color: 'white',
       }}
     >
-      <Container>
-        <Typography variant="h1" component="h1" gutterBottom>
+      <Box sx={{ textAlign: 'center', width: '100%'}}>
+        <Typography 
+          variant="h1" 
+          component="h1" 
+          gutterBottom
+          sx={{
+            fontWeight: 800,
+            fontSize: '5rem'
+          }}
+        >
           PIXY
         </Typography>
-        <Typography variant="h2" component="h2" gutterBottom>
+        <Typography variant="h2" 
+          component="h2" 
+          gutterBottom
+          sx={{
+            fontWeight: 800,
+            fontSize: '5rem'
+          }}>
           AI STAFF FOR YOU
         </Typography>
-        <Button variant="contained" color="primary" size="large">
-          자세히 알아보기
-        </Button>
-      </Container>
+      </Box>
     </Box>
   );
 };
